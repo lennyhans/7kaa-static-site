6f7ea6ea3dadfd40da84a3fe63bfb136ee2952e7
Lenny Andreu <landreuhs.29@gmail.com>
2021-04-08 00:13:25 -0400

    Add about 7k fans

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -15,6 +15,14 @@
 <svelte:head>
 	<title>About</title>
 </svelte:head>
+<section class="content">
+	<h1>About 7k Fans</h1>
+	<p>The Seven Kingdoms Fan-site is the gathering place for fans of
+		any of the Seven Kingdoms Series computer games by Enlight Software.
+		Here you can share details of the games and find opponents. 
+		This is also home of the community supported open source releases. 
+		You may find GPL versions of 7KAA and patches for 7K2 to improve on the longevity of this great series.</p>
+</section>
 
 {#each features as feature}
 <section class="content">
