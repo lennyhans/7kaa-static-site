62a918fe0c8aa82a5ad5c18dd8ba56a12df564f4
Lenny Andreu <landreuhs.29@gmail.com>
2021-04-13 00:31:55 -0400

    Add Community route to prevent unnecesary 404

---
+++ src/routes/community/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	export function preload() {
+	}	
+</script>
+
+<script>
+	export let content = "Content ";
+</script>
+
+
+<svelte:head>
+	<title>Community</title>
+</svelte:head>
+<section class="content">
+	<h1>Community content</h1>
+	<p>Comunity content should appear here</p>
+    <p>{content}</p>
+</section>
