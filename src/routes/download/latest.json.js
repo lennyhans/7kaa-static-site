2e7257f61c7ec1225a37351d90fbb5594f80fd28
Lenny Andreu <landreuhs.29@gmail.com>
2021-04-12 20:19:34 -0400

    Add latest wrapper to obtain the last version available

---
+++ src/routes/download/latest.json.js
@@ -0,0 +1,11 @@
+import versions from './_versions';
+const {title, slug} = versions[0];
+const contents = JSON.stringify( {title, slug} );
+
+export function get(req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+
+	res.end(contents);
+}
---
+++ src/routes/download/latest.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+    
+	export async function preload(page, session) {		
+        return this.fetch(`download/latest.json`).then(r => r.json()).then(versions => {
+            return this.redirect(302, `download/#${versions.slug}`);
+		});
+	}
+</script>
