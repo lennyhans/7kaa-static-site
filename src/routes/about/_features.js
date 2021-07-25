daefcad03ccb392aeba703e1b972f1798171ba0f
Lenny Andreu <landreuhs.29@gmail.com>
2021-07-25 16:33:04 -0400

    Update features getter from contentLoader

--- src/routes/about/_features.js
+++ src/routes/about/_features.js
@@ -1,29 +1,9 @@
-import fs from 'fs';
-import path from 'path'
-import frontMatter from 'front-matter';
-import marked from 'marked';
+import { getContentFromFolder } from '../../node_modules/contentLoader';
 
-const folderName = "./src/content/about/features"
+const folderName = "about/features"
 
-//TODO: Export fn to allow generic usage (with front-matter, grey-matter, yaml-fromat, etc)
-const getAllFeatures = () =>
-	fs.readdirSync(folderName)
-		.sort(getNumberPart)
-		.map(fileName => {
-		const feature = fs.readFileSync(path.resolve(folderName, fileName), "utf-8");
-		const { attributes, body } = frontMatter(feature);
-		const { title, slug } = attributes
-		return {
-			title, slug, html: marked(body)
-		}
-});
 
-//TODO: Export fn to allow generic usage
-const getNumberPart = (a,b) => {
-	const aNum = Number(a.split("-")[0])
-	const bNum = Number(b.split("-")[0])
-	return aNum < bNum ? -1 : 1
-}
+const getAllFeatures = () => getContentFromFolder(folderName);
 
 const features = getAllFeatures();
 
