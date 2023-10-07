23d6bc9cbfd50cc9671f07e7ea0809f232b7120d
Lenny Andreu <landreuhs.29@gmail.com>
2023-10-07 01:29:41 -0300

    Change the download index generation

--- gensite.pl
+++ gensite.pl
@@ -173,21 +173,30 @@ sub gen_download_index {
 	if (!open($fh, ">", "src/content/download/index.md")) {
 		return;
 	}
+	my $latest_version_file = "v$default_vars{game_version}";
+	my $latest_download_content = '';
+	my %latest_download_data = process_content_file("src/content/download/$latest_version_file.md");
+	$latest_download_content = $latest_download_content.init_partial("_download_entry.html", (spread_hash($latest_download_data{header}), content => $latest_download_data{body}));
 print $fh <<EOF;
 ---
-title: 'Downloads'
+title: 'Get the latest version of the game'
 ---
 
-## The latest version is [$default_vars{game_version}](v$default_vars{game_version}.html)
-
-### All versions
+${latest_download_content}
+### Not the version you are looking for?
+<details>
+  <summary>Older versions</summary>
+ 
 EOF
 	foreach my $name (sort{$b cmp $a} map{basename($_,'.md')} glob("src/content/download/*.md")) {
-		if ($name eq 'index') {
+		if ($name eq 'index' or $name eq  $latest_version_file) {
 			next;
 		}
 		print $fh "[$name]($name.html)</br>\n";
 	}
+	print $fh <<EOF;
+</details>
+EOF
 	close($fh);
 }
 
